8600c30196b7453a30578adbcb3731081c90aa08
Thida Nyein <thidanyein@yomabank.com>
2023-02-21 21:21:51 +0630

    Change Post Form

--- lib/src/ui/entry/create_post_page.dart
+++ lib/src/ui/entry/create_post_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p2p_pay/src/theme/color_theme.dart';
 
 class CreatePostPage extends StatefulWidget {
   static const String routeName = '/post';
@@ -16,9 +17,147 @@ class _CreatePostPageState extends State<CreatePostPage> {
       appBar: AppBar(
         title: const Text('Post'),
       ),
-      body: const Center(
-        child: Text("Post Create"),
-      ),
+      body: const MyStatefulWidget(),
     );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "အမျိုးအစား",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            TextField(
+                autocorrect: false,
+                autofocus: true,
+                controller: nameController,
+                decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(64),
+                    ),
+                  ),
+                  label: const Text("User Name"),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.cancel),
+                    onPressed: () => nameController.clear(),
+                  ),
+                ),
+                style: const TextStyle(color: Colors.black54)),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                style: const TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Type',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                // obscureText: true,
+                controller: passwordController,
+                style: const TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Amount',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: DropdownButtonFormField(
+                hint: const Text("Type"),
+                items: <String>['ABCD', 'BDCEF', 'CDEF', 'DEFG']
+                    .map((String value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    // child: Text(value),
+                    child: SizedBox(
+                      width: 50,
+                      child: Text(value),
+                    ),
+                  );
+                }).toList(),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 1),
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+                style: const TextStyle(color: Colors.black),
+                onChanged: (newValue) {},
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        foregroundColor: MaterialStateProperty.all(Colors.blue),
+                        shape: MaterialStateProperty
+                            .all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: const BorderSide(color: Colors.black)))),
+                    child: const Text('Cancel'),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                  height: 50, // <-- SEE HERE
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.black),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side:
+                                        const BorderSide(color: Colors.blue)))),
+                    child: const Text('Save'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
