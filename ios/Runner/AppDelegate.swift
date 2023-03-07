54a9d0e405bae7a9081a238c9933bcefe96b0ca0
Aung Kyaw Phyoe <akp.aungkyawphyoe7@gmail.com>
2023-03-07 22:49:17 +0630

    add notification config in app delegate(ios config) and pod install

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,11 +9,23 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
+      if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: {_, _ in })
+      } else {
+        let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+      }
+      application.registerForRemoteNotifications()
+      FirebaseApp.configure()
+      GeneratedPluginRegistrant.register(with: self)
 
     // TODO: Add your Google Maps API key
-    GMSServices.provideAPIKey("AIzaSyCEXq9vhvFS84WwOhpdOEJS6iBJQw6AsA8")
+    GMSServices.provideAPIKey("AIzaSyBt7Y9tRDDHhOYO8lsNRWsFBJxnNUjwUGc")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
