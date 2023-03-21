8f48565939271d118828532fa5f6524a437861aa
sawmainek <sawmainek@yomabank.com>
2023-03-21 23:55:31 +0630

    APK Update for V 2.0 - Login/Register - Dashboard UI Enhancement - Post Item UI Enhancement - Profile UI Enhancement - Search by YOU - PAY

--- lib/src/ui/search_page.dart
+++ lib/src/ui/search_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:multi_stream_builder/multi_stream_builder.dart';
+import '../theme/color_theme.dart';
 import './../blocs/provider_bloc.dart';
 import '../models/post.dart';
-import 'widgets/float_button.dart';
+import 'home_page.dart';
+import 'widgets/dropdown_text.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -14,206 +17,174 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   ProviderBloc providerBloc = ProviderBloc();
-  TextEditingController searchController = TextEditingController();
-  late List<String> filterProviders = [];
-  late List<String> filterTypes = ["1", "2"];
+  TextEditingController providerYouController = TextEditingController(text: "");
+  TextEditingController providerPayController = TextEditingController();
+  List<Provider> providers = [];
+  Error frmError = Error(null, null);
+  late bool checkFormError = false;
   @override
   void initState() {
     super.initState();
     providerBloc.fetchProviders();
   }
 
+  bool validationError() {
+    bool hasError = false;
+    if (checkFormError) {
+      clear();
+      if (providerYouController.text.isEmpty) {
+        hasError = true;
+        frmError.you = AppLocalizations.of(context)!.pls_choose_you;
+      }
+
+      if (providerPayController.text.isEmpty) {
+        hasError = true;
+        frmError.pay = AppLocalizations.of(context)!.pls_choose_pay;
+      }
+      setState(() {});
+    }
+    return hasError;
+  }
+
+  void clear() {
+    setState(() {
+      frmError.you = null;
+      frmError.pay = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16, 55, 16, 16),
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 2, right: 2),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black45,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(32),
-                ),
-                child: Row(
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black12,
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                        height: 32,
-                        width: 32,
-                        margin: const EdgeInsets.only(left: 8),
-                        child: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.black45,
-                          size: 24,
-                        ),
+        backgroundColor: Colors.yellow.shade200,
+        body: Stack(
+          children: [
+            Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset("assets/images/you-pay-left-top.png")),
+            Positioned(
+                right: 0,
+                bottom: 70,
+                child: Image.asset("assets/images/you-pay-bottom-right.png")),
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 120),
+                      width: 90,
+                      height: 90,
+                      decoration: const BoxDecoration(
+                        color: AppColor.secondaryColor,
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
                       ),
-                    ),
-                    Expanded(
-                      child: TextField(
-                        keyboardType: TextInputType.text,
-                        autofocus: true,
-                        controller: searchController,
-                        textInputAction: TextInputAction.search,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: AppLocalizations.of(context)!.search_here,
-                          hintStyle: const TextStyle(color: Colors.grey),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 4, horizontal: 16),
-                          isDense: true,
-                        ),
-                        onSubmitted: (_) {
-                          Navigator.pop(context, [
-                            searchController.text,
-                            filterProviders,
-                            filterTypes
-                          ]);
-                        },
-                        style:
-                            Theme.of(context).textTheme.titleMedium!.copyWith(
-                                  color: Colors.black,
-                                ),
-                      ),
-                    )
-                  ],
+                      child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Image.asset("assets/images/you-pay.png",
+                              width: 50))),
                 ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 46,
-                child: StreamBuilder<List<Provider>>(
-                    stream: providerBloc.providers,
-                    builder: (context, AsyncSnapshot<List<Provider>> snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView(
-                          scrollDirection: Axis.horizontal,
-                          clipBehavior: Clip.none,
-                          children: snapshot.data!
-                              .map((type) => FloatButton(
-                                    title: type.name,
-                                    icon: type.icon.url,
-                                    onSelected: (selected, value) {
-                                      if (selected) {
-                                        filterProviders
-                                            .add(value.replaceAll("Pay", ""));
-                                      } else {
-                                        filterProviders.remove(
-                                            value.replaceAll("Pay", ""));
-                                      }
-                                    },
-                                  ))
-                              .toList(),
-                        );
-                      }
-                      return Container();
-                    }),
-              ),
-              const SizedBox(height: 8),
-              // SizedBox(
-              //   height: 55,
-              //   child: ListView(
-              //       scrollDirection: Axis.horizontal,
-              //       clipBehavior: Clip.none,
-              //       children: [
-              //         FloatButton(
-              //           title: AppLocalizations.of(context)!.cash_out,
-              //           selected: true,
-              //           onSelected: (selected, value) {
-              //             if (selected) {
-              //               filterTypes.add("1");
-              //             } else {
-              //               filterTypes.remove("1");
-              //             }
-              //           },
-              //         ),
-              //         FloatButton(
-              //           title: AppLocalizations.of(context)!.exchange_money,
-              //           selected: true,
-              //           onSelected: (selected, value) {
-              //             if (selected) {
-              //               filterTypes.add("2");
-              //             } else {
-              //               filterTypes.remove("2");
-              //             }
-              //           },
-              //         )
-              //       ]),
-              // ),
-              /*Container(
-                margin: const EdgeInsets.fromLTRB(6, 16, 6, 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      AppLocalizations.of(context)!.recent_search,
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: Colors.black45, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              StreamBuilder(
-                  stream: providerBloc.providers,
-                  builder: (context, AsyncSnapshot snapshot) {
-                    if (snapshot.hasData) {
-                      return Wrap(
-                        alignment: WrapAlignment.start,
-                        runAlignment: WrapAlignment.start,
-                        textDirection: TextDirection.ltr,
-                        direction: Axis.horizontal,
-                        runSpacing: 8.0,
-                        spacing: 8.0,
-                        children: [
-                          ...snapshot.data!
-                              .map((type) => Container(
-                                    decoration: BoxDecoration(
-                                      color: const Color.fromARGB(
-                                          105, 128, 223, 255),
-                                      border: Border.all(
-                                        color: const Color.fromARGB(
-                                            105, 128, 223, 255),
-                                        width: 1.0,
-                                      ),
-                                      borderRadius: BorderRadius.circular(32),
-                                    ),
-                                    height: 32,
-                                    child: Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          10, 5, 10, 5),
+                const SizedBox(height: 100),
+                Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                        width: 300,
+                        child: MultiStreamBuilder(
+                            streams: [providerBloc.providers],
+                            builder: (context, dataList) {
+                              if (dataList[0] != null) {
+                                providers = dataList[0];
+                                return Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Flexible(
+                                        child: DropDownText(
+                                      label: AppLocalizations.of(context)!.you,
+                                      placeholder: AppLocalizations.of(context)!
+                                          .choose_you,
+                                      controller: providerYouController,
+                                      errorText: frmError.you,
+                                      items: dataList[0],
+                                    )),
+                                    const SizedBox(width: 16),
+                                    Flexible(
+                                        child: DropDownText(
+                                            label: AppLocalizations.of(context)!
+                                                .pay,
+                                            placeholder:
+                                                AppLocalizations.of(context)!
+                                                    .choose_pay,
+                                            controller: providerPayController,
+                                            errorText: frmError.pay,
+                                            items: dataList[0]))
+                                  ],
+                                );
+                              }
+                              return Container();
+                            }))),
+                const SizedBox(height: 50),
+                Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                        width: 300,
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: SizedBox(
+                                    height: 50,
+                                    child: ElevatedButton(
+                                      style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  AppColor.primaryColor),
+                                          foregroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  AppColor.primaryColor),
+                                          shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(32.0),
+                                          ))),
+                                      onPressed: () {
+                                        checkFormError = true;
+                                        if (!validationError()) {
+                                          List<Provider> you = providers
+                                              .where((x) =>
+                                                  x.id.toString() ==
+                                                  providerYouController.text)
+                                              .toList();
+                                          List<Provider> pay = providers
+                                              .where((x) =>
+                                                  x.id.toString() ==
+                                                  providerPayController.text)
+                                              .toList();
+                                          Navigator.pushReplacement(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      HomePage(
+                                                          you: you[0],
+                                                          pay: pay[0])));
+                                        }
+                                      },
                                       child: Text(
-                                        type.name,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleMedium!
-                                            .copyWith(
-                                              color: Colors.black45,
-                                            ),
-                                      ),
-                                    ),
-                                  ))
-                              .toList(),
-                        ],
-                      );
-                    }
-                    return Container();
-                  })*/
-            ],
-          ),
-        )
-      ]),
-    );
+                                          AppLocalizations.of(context)!.search,
+                                          style: const TextStyle(
+                                              color: Colors.white)),
+                                    )))
+                          ],
+                        ))),
+              ],
+            )
+          ],
+        ));
   }
+}
+
+class Error {
+  late String? you;
+  late String? pay;
+
+  Error(this.you, this.pay);
 }
